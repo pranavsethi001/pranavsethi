55811d3bfa188f66d70667b9fda4481228b18c96
pranavsethi001 <69353008+pranavsethi001@users.noreply.github.com>
2020-08-08 08:41:33 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+//variable setting
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
+
+// check input fields
+if (empty($name) || empty($email) || empty($message))
+{
+    echo 'Please fill all the fields.';
+}
+else
+{
+    mail("pranavsethi1000@gmail.com", "Website Message", $message, "From: $name <$email>");
+    echo "<script type = 'text/javascript'> alert('Thanks for sending your message.');
+    window.history.go(-1);
+    </script>";
+    
+}
+?>
